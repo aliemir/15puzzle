32f4f55ba4b9e2ad945e083993eac6c33d11f84c
Ali Emir Sen <senaliemir@gmail.com>
2020-05-06 03:48:25 +0300

    enzyme and styled components setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+// theme.ts
+import { DefaultTheme } from 'styled-components'
+
+const theme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+}
+
+export { theme }

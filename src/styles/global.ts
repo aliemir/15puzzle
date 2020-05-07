b970d9a9d4d6d82299bb0125ad3181c86c1a363e
Ali Emir Sen <senaliemir@gmail.com>
2020-05-07 22:03:24 +0300

    global style and theme colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,10 +2,13 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    background: ${(props) => props.theme.colors.background};
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: sans-serif;
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 700;
+    color: ${(props) => props.theme.colors.white};
   }
 `
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,17 @@
 // theme.ts
 import { DefaultTheme } from 'styled-components'
+import { invert } from '../utils/color'
 
 const theme: DefaultTheme = {
-  borderRadius: '5px',
+  borderRadius: '6px',
 
   colors: {
-    main: 'cyan',
-    secondary: 'magenta',
+    background: '#d8c593',
+    backgroundDark: '#708160',
+    primary: '#dd7631',
+    secondary: invert('#dd7631'),
+    // secondary: '#bb3b0e',
+    white: '#ffffff',
   },
 }
 
